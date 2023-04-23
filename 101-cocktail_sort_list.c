70c82b8e22da4810e646ca670fbd7b35dc61f97e
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-04-23 09:34:15 +0000

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,82 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
-
+/**
+ * cocktail_sort_list - sorts a doubly linked list of integers in ascending
+ *                     order using the Cocktail shaker sort algorithm
+ *
+ * @list: doubly linked list of integers
+ */
 void cocktail_sort_list(listint_t **list)
 {
+    listint_t *temp;
+    int swapped;
+
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
-    listint_t *left = *list, *right = *list, *cur = NULL, *tmp = NULL;
-    while (right->next != NULL)
-        right = right->next;
-    while (left != right) {
-        for (cur = left; cur != right; cur = cur->next) {
-            if (cur->n > cur->next->n) {
-                tmp = cur->next;
-                if (cur->prev != NULL)
-                    cur->prev->next = tmp;
+
+    do {
+        swapped = 0;
+
+        for (temp = *list; temp->next != NULL; temp = temp->next)
+        {
+            if (temp->n > temp->next->n)
+            {
+                temp->next->prev = temp->prev;
+                if (temp->prev)
+                    temp->prev->next = temp->next;
                 else
-                    *list = tmp;
-                tmp->prev = cur->prev;
-                cur->next = tmp->next;
-                if (tmp->next != NULL)
-                    tmp->next->prev = cur;
-                cur->prev = tmp;
-                tmp->next = cur;
+                    *list = temp->next;
+                temp->prev = temp->next;
+                temp->next = temp->next->next;
+                temp->prev->next = temp;
+                if (temp->next)
+                    temp->next->prev = temp;
+                swapped = 1;
                 print_list(*list);
             }
         }
-        right = cur = cur->prev;
-        for (; cur != left; cur = cur->prev) {
-            if (cur->n < cur->prev->n) {
-                tmp = cur->prev;
-                if (cur->next != NULL)
-                    cur->next->prev = tmp;
+
+        if (swapped == 0)
+            break;
+
+        swapped = 0;
+
+        for (temp = temp->prev; temp->prev != NULL; temp = temp->prev)
+        {
+            if (temp->n < temp->prev->n)
+            {
+                temp->prev->next = temp->next;
+                if (temp->next)
+                    temp->next->prev = temp->prev;
+                temp->next = temp->prev;
+                temp->prev = temp->prev->prev;
+                temp->next->prev = temp;
+                if (temp->prev)
+                    temp->prev->next = temp;
                 else
-                    (*list)->prev = tmp;
-                tmp->next = cur->next;
-                cur->prev = tmp->prev;
-                if (tmp->prev != NULL)
-                    tmp->prev->next = cur;
-                else
-                    *list = cur;
-                tmp->prev = cur;
-                cur->next = tmp;
+                    *list = temp;
+                swapped = 1;
                 print_list(*list);
             }
         }
-        left = cur = cur->next;
-    }
-}
-
-listint_t *create_listint(const int *array, size_t size)
-{
-    listint_t *list = NULL, *node = NULL;
-
-    while (size-- > 0) {
-        node = malloc(sizeof(listint_t));
-        if (node == NULL)
-            return (NULL);
-        node->n = array[size];
-        node->prev = NULL;
-        node->next = list;
-        if (list != NULL)
-            list->prev = node;
-        list = node;
-    }
-    return (list);
-}
-
-void print_list(const listint_t *list)
-{
-    if (list == NULL)
-        return;
-    while (list->prev != NULL)
-        list = list->prev;
-    printf("%d", list->n);
-    while (list->next != NULL) {
-        list = list->next;
-        printf(", %d", list->n);
-    }
-    printf("\n");
+    } while (swapped);
 }
