721a8f35fcc46a2dcb9cbbb37e5a7e35c9cc7d04
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-04-22 18:29:28 -0700

    105 update

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -11,42 +11,43 @@
 void radix_sort(int *array, size_t size)
 {
 	int max_value = array[0], exp = 1, *bucket = NULL;
-       	size_t i;
-	
+	size_t i;
+
 	if (array == NULL || size < 2)
 		return;
+
 	bucket = malloc(sizeof(int) * size);
-	
 	if (bucket == NULL)
-        	return;
-	
+		return;
+
 	for (i = 0; i < size; i++)
 	{
 		if (array[i] > max_value)
 			max_value = array[i];
 	}
-	
+
 	while (max_value / exp > 0)
 	{
 		int bucket_index[10] = {0};
+
 		for (i = 0; i < size; i++)
 			bucket_index[array[i] / exp % 10]++;
 
-       		for (i = 1; i < 10; i++)
-            		bucket_index[i] += bucket_index[i - 1];
+		for (i = 1; i < 10; i++)
+			 bucket_index[i] += bucket_index[i - 1];
 
-       	 	for (i = size - 1; (int) i >= 0; i--)
+		for (i = size - 1; (int) i >= 0; i--)
 		{
 			bucket[--bucket_index[array[i] / exp % 10]] = array[i];
 		}
-	       
+
 		for (i = 0; i < size; i++)
 			array[i] = bucket[i];
-		
+
 		print_array(array, size);
-		
+
 		exp *= 10;
 	}
-	
+
 	free(bucket);
 }
